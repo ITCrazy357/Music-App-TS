1476688f478f43ecf45ff983b52dad28ecb519ec
Minh Dat <ngominhdat2004@gmail.com>
2026-05-13 23:07:48 +0700

    2. Tạo router

--- index.ts
+++ index.ts
@@ -1,28 +1,18 @@
 import express, { Express, Request, Response } from "express";
 import * as database from "./config/database";
 import dotenv from "dotenv";
-
-import Topic from "./models/topic.model";
+import clientRoutes from "./routes/client/index.route";
 
 dotenv.config();
 database.connect();
 
 const app: Express = express();
-
 const port: number | string = process.env.PORT || 3000;
 
 app.set("views", "./views");
 app.set("view engine", "pug");
-
-app.get("/topics", async (req: Request, res: Response) => {
-  const topics = await Topic.find({
-    deleted: false,
-  });
-
-  console.log(topics);
-
-  res.render("client/pages/topics/index");
-});
+//client router
+clientRoutes(app);
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
